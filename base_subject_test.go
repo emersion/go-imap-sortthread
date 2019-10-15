367946e07e4ebad3f5ad3e7f0b73f6506a907314
Kevin Kuehler <keur@xcf.berkeley.edu>
2019-10-15 13:37:45 -0700

    Add isReplyFwd flag to GetBaseSubject
    The REFERENCES Algorithm requires not only getting the base subject from
    the message, but using that subject to determine whether the message is
    a reply or a forward.

--- base_subject_test.go
+++ base_subject_test.go
@@ -3,41 +3,52 @@ package sortthread
 import "testing"
 
 var baseSubjectTests = []struct {
-	name     string
-	subject  string
-	expected string
+	name       string
+	subject    string
+	expected   string
+	isReplyFwd bool
 }{
 	{
-		name:     "simple",
-		subject:  "No Replacement",
-		expected: "No Replacement",
+		name:       "simple",
+		subject:    "No Replacement",
+		expected:   "No Replacement",
+		isReplyFwd: false,
 	},
 	{
-		name:     "reply",
-		subject:  "Re: [ocf/puppet] Fix kerberos not booting up correctly [needs testing] (#781)",
-		expected: "[ocf/puppet] Fix kerberos not booting up correctly [needs testing] (#781)",
+		name:       "reply",
+		subject:    "Re: [ocf/puppet] Fix kerberos not booting up correctly [needs testing] (#781)",
+		expected:   "[ocf/puppet] Fix kerberos not booting up correctly [needs testing] (#781)",
+		isReplyFwd: true,
 	},
 	{
-		name:     "forward",
-		subject:  "Fwd: waifus",
-		expected: "waifus",
+		name:       "forward",
+		subject:    "Fwd: waifus",
+		expected:   "waifus",
+		isReplyFwd: true,
 	},
 	{
-		name:     "forward_reply",
-		subject:  "Fwd: Re: ugh",
-		expected: "ugh",
+		name:       "forward_reply",
+		subject:    "Fwd: Re: ugh",
+		expected:   "ugh",
+		isReplyFwd: true,
 	},
 }
 
 func TestBaseSubject(t *testing.T) {
 	for _, test := range baseSubjectTests {
 		t.Run(test.name, func(t *testing.T) {
-			baseSubject, err := GetBaseSubject(test.subject)
+			var isReplyFwd bool
+			baseSubject, err := GetBaseSubject(test.subject, &isReplyFwd)
 			if err != nil {
 				t.Error("Expected no error while parsing subject but got:", err)
 			}
 			if baseSubject != test.expected {
 				t.Errorf("Got %s, Expected %s.", baseSubject, test.expected)
+			}
+			if !isReplyFwd && test.isReplyFwd {
+				t.Errorf("Subject %s should be flagged as reply or forward", test.subject)
+			} else if isReplyFwd && !test.isReplyFwd {
+				t.Errorf("Subject %s was incorrectly flagged as reply or forward", test.subject)
 			}
 		})
 	}
--- sortthread.go
+++ sortthread.go
@@ -80,33 +80,42 @@ var (
 	// subj-refwd      = ("re" / ("fw" ["d"])) *WSP [subj-blob] ":"
 	subjReFwd = fmt.Sprintf(`(?:(?:re)|(?:fwd?))\s*(?:%s)?:`, subjBlob)
 	// subj-leader     = (*subj-blob subj-refwd) / WSP
-	subjLeader = regexp.MustCompile(fmt.Sprintf(`(?i)^(?:(?:(?:%s)*%s)|\s)`,
+	subjLeader = regexp.MustCompile(fmt.Sprintf(`(?i)^(?:(?:%s)*%s)`,
 		subjBlob, subjReFwd))
 	// subj-trailer    = "(fwd)" / WSP
-	subjTrailer = regexp.MustCompile(`(?i)\(fwd\)|\s$`)
+	subjTrailer = regexp.MustCompile(`(?i)\(fwd\)$`)
 )
 
 // Steps 2-4 in RFC Section 2.1
-func replaceArtifacts(subject string) string {
+func replaceArtifacts(subject string, isReplyFwd *bool) string {
 	// (2) Remove all trailing text of the subject that matches the
 	// subj-trailer ABNF; repeat until no more matches are possible.
 	for {
-		noTrail := subjTrailer.ReplaceAllString(subject, "")
+		noTrail := strings.TrimSuffix(subject, " ")
+		if subjTrailer.MatchString(noTrail) {
+
+			noTrail = subjTrailer.ReplaceAllString(noTrail, "")
+			*isReplyFwd = true
+		}
 		if subject == noTrail {
 			break
 		}
 		subject = noTrail
 	}
-	return replacePrefix(subject)
+	return replacePrefix(subject, isReplyFwd)
 }
 
 // Steps 3 & 4 in RFC Section 2.1
-func replacePrefix(subject string) string {
+func replacePrefix(subject string, isReplyFwd *bool) string {
 	// Repeat (3) and (4) until no matches remain.
 	for {
 		// (3) Remove all prefix text of the subject that matches the subj-
 		// leader ABNF.
-		noLeader := subjLeader.ReplaceAllString(subject, "")
+		noLeader := strings.TrimPrefix(subject, " ")
+		if subjLeader.MatchString(noLeader) {
+			noLeader = subjLeader.ReplaceAllString(noLeader, "")
+			*isReplyFwd = true
+		}
 
 		// (4) If there is prefix text of the subject that matches the subj-
 		// blob ABNF, and removing that prefix leaves a non-empty subj-
@@ -124,11 +133,13 @@ func replacePrefix(subject string) string {
 	return subject
 }
 
-func GetBaseSubject(subject string) (string, error) {
+func GetBaseSubject(subject string, isReplyFwd *bool) (string, error) {
 	var (
 		baseSubject string
 		err         error
 	)
+
+	*isReplyFwd = false
 
 	// (1) Convert any RFC 2047 encoded-words in the subject to [UTF-8]
 	// as described in "Internationalization Considerations".
@@ -145,7 +156,7 @@ func GetBaseSubject(subject string) (string, error) {
 
 	for {
 		// Steps 2-5
-		baseSubject = replaceArtifacts(baseSubject)
+		baseSubject = replaceArtifacts(baseSubject, isReplyFwd)
 
 		// (6) If the resulting text begins with the subj-fwd-hdr ABNF and
 		// ends with the subj-fwd-trl ABNF, remove the subj-fwd-hdr and
@@ -153,6 +164,7 @@ func GetBaseSubject(subject string) (string, error) {
 		if !strings.HasPrefix(baseSubject, subjFwdHeader) || !strings.HasSuffix(baseSubject, subjFwdTrail) {
 			break
 		}
+		*isReplyFwd = true
 		baseSubject = strings.TrimPrefix(baseSubject, subjFwdHeader)
 		baseSubject = strings.TrimSuffix(baseSubject, subjFwdTrail)
 	}
